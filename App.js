77d53c97048417d212e07373ce693d574d2c6075
weiyuyu <weiyuyu@usc.edu>
2018-05-29 18:12:43 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+
+import Store from './src/Store';
+import { Header, CryptoContainer } from './src/components';
+
+//Main App Component
+
+export default class App extends Component {
+  render() {
+    return (
+      <Provider store={Store}>
+        <View>
+          <Header />
+          <CryptoContainer />
+        </View>
+      </Provider>
+    );
+  }
+}
